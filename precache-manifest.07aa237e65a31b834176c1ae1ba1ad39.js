45fb44f4eb777c78438db843f47b38579bac1453
alex <abrackx@gmail.com>
2020-07-04 13:27:34 -0400

    Updates

---
+++ precache-manifest.07aa237e65a31b834176c1ae1ba1ad39.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62adfc3b627bd315b565f7ff25ba5055",
+    "url": "/brahts-ui/index.html"
+  },
+  {
+    "revision": "f2e1e0c151b83ac721c9",
+    "url": "/brahts-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2ca25b1d23aa31a19ae",
+    "url": "/brahts-ui/static/js/2.fa9348d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/brahts-ui/static/js/2.fa9348d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e1e0c151b83ac721c9",
+    "url": "/brahts-ui/static/js/main.df826b1e.chunk.js"
+  },
+  {
+    "revision": "079b9de7052e09c6d189",
+    "url": "/brahts-ui/static/js/runtime-main.b213cc75.js"
+  },
+  {
+    "revision": "c4ae6596f564d0852c4948598315e528",
+    "url": "/brahts-ui/static/media/db.c4ae6596.svg"
+  },
+  {
+    "revision": "90a9652af99c38166957472668edeb8b",
+    "url": "/brahts-ui/static/media/email.90a9652a.svg"
+  },
+  {
+    "revision": "9b6c7cf43d21f9a7a3c7ddd377d23507",
+    "url": "/brahts-ui/static/media/java.9b6c7cf4.svg"
+  },
+  {
+    "revision": "717a48c25421c26ef198e352175129e3",
+    "url": "/brahts-ui/static/media/kafka.717a48c2.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brahts-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "edf462fec682b9d48cf628eaf9e19521",
+    "url": "/brahts-ui/static/media/spring.edf462fe.svg"
+  },
+  {
+    "revision": "76d7c4727146bbfd83e27df5f7963660",
+    "url": "/brahts-ui/static/media/usf.76d7c472.svg"
+  }
+]);
